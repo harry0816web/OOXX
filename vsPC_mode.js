f22867bfadb8ab30151473bfaf95173e302e7681
unknown <harry940816@gmail.com>
2021-07-09 13:03:04 +0800

    'fixBug'

--- vsPC_mode.js
+++ vsPC_mode.js
@@ -2,7 +2,6 @@ var FirstManTurn = true;
 var PlayerTurn = true;
 var WhoWinWhereRL,WhoWinWhereTD;                                                                                    
 var align = false;
-var turns = 1;
 var alreadyAlignStop = false;
 var canPutChest = false;
 function PlayerChoose(thisBlock){
@@ -15,67 +14,67 @@ function PlayerChoose(thisBlock){
             alert('O 已經贏了');
         }
     }
-    putChest(thisBlock);
-    VSpc();
+    else if(canPutChest && !alreadyAlignStop){
+        putChest(thisBlock); 
+        VSpc();
+    }
+    canPutChest = false;    
 }
 function putChest(thisBlock){
-    if(!alreadyAlignStop){
-        if(FirstManTurn && canPutChest){
-            thisBlock.innerHTML = 'O';
-            FirstManTurn = false;
-        }
-        else if(!FirstManTurn && canPutChest){
-            thisBlock.innerHTML = 'X';
-            FirstManTurn = true;
-        }
-        else{
-            alert("這格是"+thisBlock.innerHTML); 
-        }
-    }
+    if(FirstManTurn && canPutChest){
+           thisBlock.innerHTML = 'O';
+         FirstManTurn = false;
+     }
+    else if(!FirstManTurn && canPutChest){
+          thisBlock.innerHTML = 'X';
+         FirstManTurn = true;
+      }
+     else{
+         alert("這格是"+thisBlock.innerHTML); 
+     }
     CheckWinner(thisBlock);
 }
 function CheckWinner(thisBlock){
     var debug;
-        if(playerWin() == "LeftToRight"){
-            alert(thisBlock.innerHTML + "在" + WhoWinWhereRL + '橫列獲勝');
-            align = true;
-            debug = 1;
-        }
-        else if(playerWin() == "TopToDown"){
-            alert(thisBlock.innerHTML + "在" + WhoWinWhereTD + '直列獲勝');
-            align = true;
-            debug = 2;
-        }
-        else if(playerWin() == "LeftTopToRightDown"){
-            alert(thisBlock.innerHTML + "左上至右下斜線獲勝");
-            align = true;
-            debug = 3;
-        }
-        else if(playerWin() == "RightTopToLeftDown") {
-            alert(thisBlock.innerHTML + "右上至左下斜線獲勝");
-            align = true;
-            debug = 4;
-        }  
-        if(align){
-            align = false;
-            alreadyAlignStop = true;
+     if(playerWin() == "LeftToRight"){
+         alert(thisBlock.innerHTML + "在" + WhoWinWhereRL + '橫列獲勝');
+         align = true;
+        debug = 1;
+     }
+     else if(playerWin() == "TopToDown"){
+        alert(thisBlock.innerHTML + "在" + WhoWinWhereTD + '直列獲勝');
+        align = true;
+         debug = 2;
+     }
+     else if(playerWin() == "LeftTopToRightDown"){
+         alert(thisBlock.innerHTML + "左上至右下斜線獲勝");
+        align = true;
+         debug = 3;
+    }
+     else if(playerWin() == "RightTopToLeftDown") {
+         alert(thisBlock.innerHTML + "右上至左下斜線獲勝");
+         align = true;
+        debug = 4;
+     }  
+     if(align){
+         align = false;
+         alreadyAlignStop = true;
+         document.getElementById('restart').innerHTML = "press 'G' to restart";
+         var background = document.querySelector('.BG');
+         if(FirstManTurn){
+             background.style = "background-color:darkgray";
+         }
+         else{
+             background.style = "background-color: rgb(220, 238, 255);";
+         }
+     }
+     else{
+        if(CheckifTheMapisFull()){
+            alert('已無空位');
+            console.log(thisBlock.innerHTML);
             document.getElementById('restart').innerHTML = "press 'G' to restart";
-            var background = document.querySelector('.BG');
-            if(FirstManTurn){
-                background.style = "background-color:darkgray";
-            }
-            else{
-                background.style = "background-color: rgb(220, 238, 255);";
-            }
-        }
-        else{
-            if(CheckifTheMapisFull()){
-                alert('已無空位');
-                console.log(thisBlock.innerHTML);
-                document.getElementById('restart').innerHTML = "press 'G' to restart";
-            }
-        }
-        console.log(thisBlock.innerHTML+'//'+turns);
+         }
+     }
 }
 //ifPlarerWin
 function playerWin(){
@@ -95,7 +94,6 @@ function playerWin(){
             if(character[i][0] == character[i][1] && character[i][1] == character[i][2]){
                 WhoWinWhereRL = '第' + column;
                 return "LeftToRight";
-                break;
             }
         }
         column++;
@@ -107,7 +105,6 @@ function playerWin(){
              if(character[0][i] == character[1][i] && character[1][i] == character[2][i]){
                  WhoWinWhereTD = '第' + row;
                  return "TopToDown";
-                 break;
              }
         }
         row++;
@@ -134,7 +131,6 @@ function clearMap(){
     }
     FirstManTurn = true;
     document.getElementById('restart').innerHTML = '';
-    turns = 1;
     document.querySelector('.BG').style="background-color: cornsilk;";
     alreadyAlignStop = false;
 }
@@ -143,25 +139,11 @@ function restartHotKey(){
     if (event.keyCode==71) {
 		clearMap();
 	}
-    // else if(event.keyCode==84){
-
-    // }
 }
 document.onkeydown=restartHotKey;
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-// function VSpc(){                                                                                    
-//     var row,column;
-//     do{
-//         row = getRandomInt(3);column = getRandomInt(3);
-//         if(character[row][column] != 'O' && character[row][column] != 'X')break;
-//     }while(character[row][column] !=  'O' && character[row][column] != 'X');
-//     if(FirstManTurn){
-//         character[row][column] = 'O';
-//     }
-//     else character[row][column] = 'X';
-// }
 function VSpc(){
     if(!alreadyAlignStop && !CheckifTheMapisFull()){
         var IdOfTheBlock;
@@ -194,7 +176,6 @@ function CheckifTheMapisFull(){
             IdOfTheBlock = "map" + i + j;
             if(document.getElementById(IdOfTheBlock).innerHTML != 'O' && document.getElementById(IdOfTheBlock).innerHTML != 'X'){
                 return false;
-                break;
             }
         }
     }
